1aebaa9cdf62d51f60370431b3fb956a222a2359
cealyssa814 <a.hollingsworth814@gmail.com>
2025-10-10 16:40:52 -0400

    Created first demo of transaction list with file reader

--- src/main/java/com/pluralsight/GymLedger.java
+++ src/main/java/com/pluralsight/GymLedger.java
@@ -1,7 +1,32 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class GymLedger {
-        public static void main(String[] args) {
-            System.out.println("Starting off strong");
+    private static final String FILE_NAME = "transactions.csv";
+    private static final ArrayList<Transactions> transactions = new ArrayList<>();
+
+    public static void main(String[] args) {
+        loadTransactions();
+        System.out.println("~~~ Transactions (with demo data added) ~~~");
+        for (int i = 0; i < transactions.size(); i++) {
+            System.out.println(transactions.get(i));
+        }
+        System.out.println("Total entries: " + transactions.size());
+    }
+
+    private static void loadTransactions() {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Transactions t = Transactions.fromCsv(line.trim());
+                if (t != null) transactions.add(t);
+            }
+            System.out.println("(Loaded " + transactions.size() + " transactions)");
+        } catch (IOException e) {
+            System.out.println("Problem reading file");
         }
     }
+}
+
