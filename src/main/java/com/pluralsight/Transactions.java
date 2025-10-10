1ebdac0a0d90fe14f37eb502b06bbd61a657cf00
cealyssa814 <a.hollingsworth814@gmail.com>
2025-10-10 10:39:12 -0400

    Added Transaction class data added

--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,10 +1,10 @@
 package com.pluralsight;
 
 public class Transactions {
-    // fields match the CSV columns
-    private String date;        // YYYY-MM-DD
-    private String time;        // HH:MM:SS
-    private String description; // what happened
-    private String vendor;      // who it was with
-    private double amount;      // + for deposits, - for payments
+
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
 
