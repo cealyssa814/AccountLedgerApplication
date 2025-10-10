1ad1b95bd3eaeacc9cb714458d49c213d8b0e69b
cealyssa814 <a.hollingsworth814@gmail.com>
2025-10-10 11:09:15 -0400

    Added getters and setters to the Transaction constructor

--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -8,3 +8,23 @@ public class Transactions {
     private String vendor;
     private double amount;
 
+    public Transactions(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate()        { return date; }
+    public String getTime()        { return time; }
+    public String getDescription() { return description; }
+    public String getVendor()      { return vendor; }
+    public double getAmount()      { return amount; }
+
+    public void setDate(String date)              { this.date = date; }
+    public void setTime(String time)              { this.time = time; }
+    public void setDescription(String description){ this.description = description; }
+    public void setVendor(String vendor)          { this.vendor = vendor; }
+    public void setAmount(double amount)          { this.amount = amount; }
+
